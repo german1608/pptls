cc821abbd17594a4496106b56f10cf73cc65af4e
German Robayo Paz <germanrobayo33@gmail.com>
2018-12-24 02:22:01 -0400

    documenta Estrategias.rb

--- Estrategias.rb
+++ Estrategias.rb
@@ -1,9 +1,28 @@
 load 'Jugadas.rb'
 
+##
+# Cada jugador es representado con una estrategia
+# Esta clase provee una interfaz sencilla para poder
+# implementar estrategias personalizadas por el usuario
+# final
+#
+# +SEMILLA+ :: Semilla para números aleatorios
 class Estrategia
-    attr_accessor :jugador, :SEMILLA
+    ##
+    # jugador :: Nombre del jugador de la estrategia
+    attr_accessor :jugador
+
+    ##
+    # SEMILLA :: Semilla para generar los números pseudo-aleatorios.
+    attr_accessor :SEMILLA
     SEMILLA = 42
 
+    ##
+    # Determina la jugada del turno actual. Puede utilizar
+    # el historial de jugadas y el parámetro actual.
+    #
+    # Parámetros:
+    # - +jugada_pasada+:: +Jugada+ realizada por el adversario en la ronda anterior
     def prox(jugada_pasada=nil)
         if !(jugada_pasada == nil or
             (jugada_pasada.is_a? Jugada and jugada_pasada.class != Jugada))
@@ -11,14 +30,20 @@ class Estrategia
         end
     end
 
+    ##
+    # Nombre del jugador de la estrategia
     def initialize(jugador=nil)
         @jugador = jugador
     end
 
+    ##
+    # Retorna la estrategia como un +String+.
     def to_s
         "Jugador: #{@jugador} - Estrategia: #{self.class} "
     end
 
+    ##
+    # Reinicia el estado de la estrategia.
     def reset
     end
 
@@ -27,10 +52,23 @@ class Estrategia
     end
 end
 
+##
+# La estrategia manual requiere de un usuario real que escoga
+# que desea jugar. Es de las estrategias más simples requeridas.
 class Manual < Estrategia
 
+    ##
+    # El prox de esta estrategia es sencillamente jugar lo que
+    # el usuario decida. Pedimos input por STDIN (aunque en
+    # la interfaz resolvemos esto usando un input mocker).
+    #
+    # Parámetros
+    # - +jugada_pasada+: No se usa, pero se deja para que se sobreescriba
+    # el método de la superclase
     def prox(jugada_pasada)
         super jugada_pasada
+
+        # Pedimos input hasta que sea una opción posible
         opcion = ""
         opciones_posibles = ["1", "2", "3", "4", "5"]
         while !(opciones_posibles.include? opcion) do
@@ -42,6 +80,9 @@ class Manual < Estrategia
                     5- Spock")
             opcion = gets.to_s
         end
+
+        # Este hash lookup nos permite evitar el uso
+        # engorroso de if's.
         inputs_para_manual = {
             "1" => Piedra,
             "2" => Papel,
@@ -53,36 +94,64 @@ class Manual < Estrategia
     end
 end
 
+##
+# La estrategia Copiar consiste en copiar la jugada
+# realizada por el adversario en el turno pasado.
 class Copiar < Estrategia
-    attr_accessor :primeraJugada, :reiniciar
-
+    ##
+    # El constructor de Copiar recibe como parámetro
+    # la jugada a realizar en el primer turno.
+    #
+    # Parámetros:
+    # - +primeraJugada+: Jugada a realizar en el primer turno.
+    # - +jugador+: (opcional) Nombre del jugador.
     def initialize(primeraJugada, jugador=nil)
         if !(primeraJugada.is_a? Jugada and primeraJugada.class != Jugada)
             raise ArgumentError.new("La estrategia de Copia debe inicializar con una subclase de Jugada")
         end
         self.jugador = jugador
-        self.primeraJugada = primeraJugada
-        self.reiniciar = false
+        @primeraJugada = primeraJugada
     end
 
+    ##
+    # El método prox de esta estrategia revisa si la primeraJugada ya se realizó.
+    # Al realizarla se iguala esta a nil para que no vuelva a ser usada (usaría
+    # siempre el argumento (la jugada del adversario en la ronda pasado).
+    #
+    # Parámetros
+    # - +jugada_pasada+: +Jugada+ realizada por el adversario en el turno anterior.
     def prox(jugada_pasada=nil)
         super jugada_pasada
-        if jugada_pasada == nil
-            @primeraJugada
-        else
+        if @primeraJugada == nil
             jugada_pasada
+        else
+            j = @primeraJugada
+            @primeraJugada = nil
+            j
         end
-    end
-
-    def reset
-        self.reiniciar = true
     end
 
 end
 
+##
+# Estrategia que recibe una lista de Símbolos a Jugadas y retorna
+# una instancia de cualquiera de estas escogiendo uniformemente
+# un número entero entre 0 y la longitud del parámetro (inclusive, exclusive)
 class Uniforme < Estrategia
-    attr_accessor :jugadas
 
+    ##
+    # El constructor fue descrito en la documentación de la clase.
+    # Se hacen los siguientes chequeos:
+    # - El argumento solo debe ser un arreglo cuyos elementos sean
+    #   - +:Tijeras+
+    #   - +:Papel+
+    #   - +:Piedra+
+    #   - +:Spock+ ó
+    #   - +:Lagarto+
+    # - El argumento debe tener al menos un elemento.
+    #
+    # Parámetros:
+    # - +jugadas+: Arreglo con las características descritas anteriormente.
     def initialize(jugadas, jugador=nil)
         posibles_jugadas = [:Tijeras, :Papel, :Piedra, :Spock, :Lagarto]
         if jugadas.size == 0
@@ -97,6 +166,13 @@ class Uniforme < Estrategia
         self.jugador = jugador
     end
 
+    ##
+    # Retorna la siguiente jugada a realizar de manera uniforme
+    # calculando un número pseudo-aleatorio perteneciente a [0, @jugadas.size)
+    #
+    # Parámetros
+    # - +jugada_pasada+: No se usa, pero se deja para poder facilitar el uso
+    # de la misma.
     def prox(jugada_pasada=nil)
         super jugada_pasada
         retornos = {
@@ -112,12 +188,36 @@ class Uniforme < Estrategia
     end
 end
 
-
+##
+# La estrategia pensar es un poco más complicada de explicar.
+# Usa el historial de jugadas (conservado como un atributo de instancia).
+# Dicho atributo solo puede ser modificado internamente por los métodos
+# de la misma.
 class Pensar < Estrategia
+    ##
+    # Inicializamos la estrategia asignando un historial vacío
     def initialize
         @historial = []
     end
 
+    ##
+    # La próxima jugada a realizar usando esta estrategia se describe de
+    # la siguiente manera:
+    # 1. Sea +numPiedras, +numPapel, +numTijeras, +numLagarto y +numSpock+
+    #    la cantidad de piedras, papel, tijeras, lagartos y spocks que ha jugado
+    #    el oponente en la partida.
+    # 2. Calculamos las sumas de las variables calculadas anteriormente.
+    # 3. Se calcula un número aleatorio entero entre 0 y el total calculado
+    #    en el paso 2.
+    # 4. La jugada será uns instancia de:
+    #    - +Piedra+, si el resultado cae en el rango <tt>[0, numPiedras)</tt>
+    #    - +Papel+, si el resultado cae en el rango <tt>[numPiedras, numPiedras + numPapel)</tt>
+    #    - +Tijeras+, si el resultado cae en el rango <tt>[numPiedras + numPapel,
+    #      numPiedras + numPapel + numTijeras)</tt>
+    #    - +Lagarto+, si el resultado cae en el rango <tt>[numPiedras + numPapel + numTijeras,
+    #      numPiedras + numPapel + numTijeras + numLagarto)</tt>
+    #    - +Spock+, si el resultado cae en el rango <tt>[numPiedras + numPapel + numTijeras +
+    #      numLagarto, numPiedras + numPapel + numTijeras + numLagarto + numSpock)</tt>
     def prox(jugada_pasada)
         super jugada_pasada
         if jugada_pasada != nil
@@ -149,12 +249,23 @@ class Pensar < Estrategia
         end
     end
 
+    ##
+    # Resetear la estrategia es equivalente a vaciar el historial
     def reset
         @historial = []
     end
 end
 
+##
+# La clase Sesgada es muy similar a Pensar. Esta usa un mapa
+# de frecuencia para cada jugada y de esta manera determinar
+# la jugada a realizar dependiendo del valor de su frecuencia
+# asociada
 class Sesgada < Estrategia
+    ##
+    # El constructor verifica que las claves del hash sean los posibles
+    # valores (mismos de +Uniforme+). y que los valores de estos
+    # sean de tipo numérico.
     def initialize probs_sesgadas
         posibles_jugadas = [:Tijeras, :Papel, :Piedra, :Spock, :Lagarto]
         simbolos_a_jugadas = {
@@ -181,6 +292,10 @@ class Sesgada < Estrategia
             end
         end
     end
+
+    ##
+    # La próxima jugada es sencillamente buscar un elemento de forma uniforme
+    # del arreglo +@jugadas_posibles+
     def prox(jugada_pasada=nil)
         super jugada_pasada
         posicion = Random.new(SEMILLA)
